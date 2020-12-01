18214c07c9fded0aa7c4bcbcdf12acd645cc4ce3
Johannes Groedem <johs@grdm.no>
2020-12-01 21:46:34 +0100

    Add a main class to project config.

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/math.combinatorics "0.1.6"]]
+  :main ^:skip-aot aoc-2020.core
   :resource-paths ["src/resources"]
   :repl-options {:init-ns aoc-2020.core})
