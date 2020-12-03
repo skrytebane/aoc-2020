704e0aae062e19b4ed2ef7fff49eba2d60d74c94
Johannes Groedem <johs@grdm.no>
2020-12-03 08:56:31 +0100

    Day 3: Working part 2.

--- src/aoc_2020/day-03.clj
+++ src/aoc_2020/day-03.clj
@@ -19,34 +19,54 @@
 (defn map-get [m x y]
   (nth (nth m y) x))
 
-(defn move
-  ([m x y] (move m x y 3 1))
-  ([m x y move-x move-y]
-   (let [[width _] (map-size m)
-         new-y (+ y move-y)
-         line (nth m new-y)
-         new-x (mod (+ x move-x) width)]
-     (vector new-x new-y))))
+(defn move [m x y move-x move-y]
+  (let [[width _] (map-size m)
+        new-y (+ y move-y)
+        line (nth m new-y)
+        new-x (mod (+ x move-x) width)]
+    (vector new-x new-y)))
 
-(defn ride [m]
-  (let [[_ height] (map-size m)]
-    (loop [x 0
-           y 0
-           trail []]
-      (if (= y (dec height))
-        trail
-        (let [[new-x new-y] (move m x y)]
-          (recur new-x new-y (conj trail (map-get m new-x new-y))))))))
+(defn ride
+  ([m] (ride m 3 1))
+  ([m right down]
+   (let [[_ height] (map-size m)]
+     (loop [x 0
+            y 0
+            trail []]
+       (if (= y (dec height))
+         trail
+         (let [[new-x new-y] (move m x y right down)]
+           (recur new-x new-y (conj trail (map-get m new-x new-y)))))))))
+
+(defn read-map [filename]
+  (->> filename slurp-lines parse-map))
+
+(defn count-ride
+  ([m] (count-ride m 3 1))
+  ([m right down]
+   (->> (ride m right down)
+        (filter #(= % \#))
+        count)))
 
 (defn solution-day03 [filename]
   (->> filename
-       slurp-lines
-       parse-map
-       ride
-       (filter #(= % \#))
-       count))
+       read-map
+       count-ride))
+
+(def alternatives [[1 1]
+                   [3 1]
+                   [5 1]
+                   [7 1]
+                   [1 2]])
+
+(defn solution-day03b [filename]
+  (let [m (read-map filename)]
+    (->> (map #(apply count-ride m %) alternatives)
+         (reduce *))))
 
 (comment
   (solution-day03 "sample-03.txt")
   (solution-day03 "input-03.txt")
+  (solution-day03b "sample-03.txt")
+  (solution-day03b "input-03.txt")
   )
