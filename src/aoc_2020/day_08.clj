59697b45d1b683e07abbc49b6a323a5d862ca8dc
Johannes Groedem <johs@grdm.no>
2020-12-08 08:57:29 +0100

    Day 8: Part 2 solved.

--- src/aoc_2020/day_08.clj
+++ src/aoc_2020/day_08.clj
@@ -9,27 +9,24 @@
 
 (defn- step [program execution]
   (let [[instruction offset] (nth program (:ip execution))
-        ip (:ip execution)
-        counters (update-in (:counters execution) [ip] inc)]
-    (case instruction
-      "nop" (assoc execution
-                   :ip (inc ip)
-                   :counters counters)
-      "acc" (assoc execution
-                   :ip (inc ip)
-                   :accumulator (+ (:accumulator execution) offset)
-                   :counters counters)
-      "jmp" (assoc execution
-                   :ip (+ ip offset)
-                   :counters counters))))
+        ip (:ip execution)]
+    (assoc (case instruction
+             "nop" (assoc execution :ip (inc ip))
+             "acc" (assoc execution
+                          :ip (inc ip)
+                          :accumulator (+ (:accumulator execution) offset))
+             "jmp" (assoc execution :ip (+ ip offset)))
+           :counters (update-in (:counters execution) [ip] inc))))
 
 (defn execute-program [program]
   (let [execution {:ip 0
                    :counters (vec (take (count program) (repeat 0)))
-                   :accumulator 0}]
+                   :accumulator 0}
+        last-ip (dec (count program))]
     (loop [execution (step program execution)
            trace [execution]]
-      (if (some #(> % 1) (:counters execution))
+      (if (or (some #(> % 1) (:counters execution))
+              (= (:ip execution) last-ip))
         trace
         (let [next-step (step program execution)]
           (recur next-step (conj trace next-step)))))))
@@ -43,10 +40,31 @@
        last
        :accumulator))
 
-(defn scratch []
-  (solution-day08 "input-08.txt"))
+(defn swap-instruction [[instruction offset]]
+  [(case instruction
+     "jmp" "nop"
+     "nop" "jmp"
+     instruction)
+   offset])
+
+(defn twiddle-program [program instruction]
+  (update-in program [instruction] swap-instruction))
+
+(defn solution-day08-b [filename]
+  (let [program (->> filename slurp-lines parse-program)
+        twiddled-programs (->> (range (count program))
+                               (filter #(contains? #{"jmp" "nop"} (first (nth program %))))
+                               (map #(twiddle-program program %)))]
+    (->> twiddled-programs
+         (map execute-program)
+         (map last)
+         (filter #(= (:ip %) (dec (count program))))
+         first
+         :accumulator)))
 
 (comment
   (solution-day08 "sample-08.txt") ;; 5
   (solution-day08 "input-08.txt") ;; 1859
+  (solution-day08-b "sample-08.txt") ;; 2
+  (solution-day08-b "input-08.txt") ;; 1235
   )
