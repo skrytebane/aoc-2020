6b9bbcfc590942c29505be95f1fd4497b8ef983f
Johannes Groedem <johs@grdm.no>
2020-12-09 09:07:07 +0100

    Day 9: Hackety hack. Part 2 solved.

--- src/aoc_2020/day_09.clj
+++ src/aoc_2020/day_09.clj
@@ -2,25 +2,51 @@
   (:require [aoc-2020.core :refer [slurp-lines]]
             [clojure.math.combinatorics :as comb]))
 
-(defn pairs [numbers]
+(defn- pairs [numbers]
   (comb/combinations numbers 2))
 
-(defn range-sums [numbers from to]
+(defn- range-sums [numbers from to]
   (->> (for [index (range from to)]
         (nth numbers index))
        pairs
        (map #(reduce + %))
        set))
 
+(defn- read-numbers [filename]
+  (->> filename
+       slurp-lines
+       (map #(Long/parseLong %))
+       vec))
+
 (defn solution-a [filename preamble-size]
-  (let [numbers (->> filename
-                     slurp-lines
-                    (map #(Long/parseLong %))
-                    vec)]
+  (let [numbers (read-numbers filename)]
     (->> (range preamble-size (count numbers))
          (remove #(contains? (range-sums numbers (- % preamble-size) %) (nth numbers %)))
          (map #(nth numbers %))
          first)))
 
+(defn- get-error-row [filename preamble-size]
+  (let [numbers (read-numbers filename)
+        invalid-number (solution-a filename preamble-size)
+        index (.indexOf numbers invalid-number)
+        test-range (subvec numbers 0 index)
+        window-sizes (range 2 (count test-range))]
+    (->> window-sizes
+         (map #(partition % 1 test-range))
+         (map #(filter (fn [x]
+                         (= (reduce + x) invalid-number))
+                       %))
+         (filter seq)
+         ffirst)))
+
+(defn solution-b [filename preamble-size]
+  (let [error-row (get-error-row filename preamble-size)]
+    (+ (reduce min error-row)
+       (reduce max error-row))))
+
 (comment
-  (solution-a "sample-09.txt" 5))
+  (solution-a "sample-09.txt" 5)
+  (solution-a "input-09.txt" 25)
+  (solution-b "sample-09.txt" 5)
+  (solution-b "input-09.txt" 25)
+  )
