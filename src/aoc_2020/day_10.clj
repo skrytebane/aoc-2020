11a824bcac3e40add037b96bb6307f2d6daf2dbe
Johannes Groedem <johs@grdm.no>
2020-12-10 12:05:12 +0100

    Day 10: Solved part 2 for samples, but not real inputs.

--- src/aoc_2020/day_10.clj
+++ src/aoc_2020/day_10.clj
@@ -1,13 +1,14 @@
 (ns aoc-2020.day-10
-  (:require [aoc-2020.core :refer [slurp-lines parse-ints]]))
+  (:require [aoc-2020.core :refer [slurp-lines parse-ints]]
+            [clojure.math.combinatorics :as comb]))
 
-(defn read-jolts [filename]
+(defn- read-jolts [filename]
   (->> filename
        slurp-lines
        parse-ints
        sort))
 
-(defn connect-adapters [adapters]
+(defn- connect-adapters [adapters]
   (loop [remaining adapters
          current-joltage 0
          connected [0]]
@@ -29,8 +30,31 @@
     (* (count (get v 1))
        (count (get v 3)))))
 
+(defn is [x]
+  (fn [y] (= x y)))
+
+(defn- valid-connections [adapters joltage]
+  (if (empty? adapters)
+    1
+    (let [eligible (filter #(<= 1 (- % joltage) 3) adapters)]
+      (if (empty? eligible)
+        1
+        (reduce + (map #(valid-connections (remove (is %) adapters) %)
+                       eligible))))))
+
+(defn solution-b [filename]
+  (as-> (read-jolts filename) v
+    (valid-connections v 0)))
+
+(defn scratch []
+  (solution-b "sample-10-a.txt"))
+
 (comment
   (solution-a "sample-10-a.txt") ;; 35
   (solution-a "sample-10-b.txt") ;; 220
   (solution-a "input-10.txt") ;; 1980
+
+  (solution-b "sample-10-a.txt") ;; 8
+  (solution-b "sample-10-b.txt") ;; 19208
+  (solution-b "input-10.txt") ;; ?
   )
