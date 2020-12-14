74478e8d6e7484469182431932a32086824a943a
Johannes Groedem <johs@grdm.no>
2020-12-14 09:27:18 +0100

    Day 14: WIP part 2.

--- src/aoc_2020/day_14.clj
+++ src/aoc_2020/day_14.clj
@@ -11,11 +11,17 @@
                          :value (Long/parseUnsignedLong value)}
       :else "ffs")))
 
+(defn- make-or-mask [mask]
+  (Long/parseLong (str/join "" (replace {\X 0} mask)) 2))
+
+(defn- make-and-mask [mask]
+  (Long/parseLong (str/join "" (replace {\X 1} mask)) 2))
+
 (defn- write-masked [memory instruction mask]
   (let [address (:address instruction)
         value (:value instruction)
-        or-mask (Long/parseLong (str/join "" (replace {\X 0} mask)) 2)
-        and-mask (Long/parseLong (str/join "" (replace {\X 1} mask)) 2)]
+        or-mask (make-or-mask mask)
+        and-mask (make-and-mask mask)]
     (assoc memory address (bit-and (bit-or value or-mask) and-mask))))
 
 (defn- write-mem [instructions]
@@ -38,6 +44,40 @@
        write-mem
        vals
        (reduce +)))
+
+(defn- compute-address [address mask expanded]
+  ;; TODO: WIP HERE
+  )
+
+(defn- update-memory [memory mask address value]
+  (let [or-mask (make-or-mask mask)
+        and-mask (make-and-mask mask)
+        address (bit-and (bit-or address or-mask) and-mask)
+        wildcards (->> mask (filter #(= % \X)) count)
+        expanded (range 0 (long (Math/pow 2 wildcards)))]
+    (loop [expanded expanded
+           memory memory]
+      (if (empty? expanded)
+        memory
+        (let [addr (compute-address address mask (first expanded))]
+          (recur (rest expanded) (assoc memory addr value)))))))
+
+(defn- count-mem [instructions]
+  (loop [instructions instructions
+         memory {}
+         mask nil]
+    (if (empty? instructions)
+      memory
+      (let [instruction (first instructions)]
+        (if (:mask instruction)
+          (recur (rest instructions) memory (:mask instruction))
+          (recur (rest instructions) (update-memory memory mask (:address instruction) (:value instruction)) mask))))))
+
+(defn solution-b [filename]
+  (->> filename
+       slurp-lines
+       (map parse-bitfiddle)
+       count-mem))
 
 (comment
   (solution-a "sample-14.txt")
