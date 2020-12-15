96c1120e68f009c9f7953fe2ce10b3548a5ccfa7
Johannes Groedem <johs@grdm.no>
2020-12-15 14:03:50 +0100

    Day 15: Clean ups.

--- src/aoc_2020/day_15.clj
+++ src/aoc_2020/day_15.clj
@@ -3,50 +3,29 @@
 (def sample [0 3 6])
 (def input [13 16 0 12 15 1])
 
-(defn- index [n v s]
-  (first
-   (for [i (reverse (range s))
-         :when (= v (nth n i))]
-     i)))
-
-(defn- gen-seq [nums stop]
-  (loop [nums nums
-         turn (count nums)]
-    (let [final (last nums)
-          prev-final (index nums final (dec turn))]
-      (if (= stop turn)
-        final
-        (if (nil? prev-final)
-          (recur (conj nums 0)
-                 (inc turn))
-          (recur (conj nums (- (dec turn) prev-final))
-                 (inc turn)))))))
-
-(defn solution-a [nums]
-  (gen-seq nums 2020))
-
-(defn- ngen-seq [nums stop]
-  (loop [turn (count nums)
-         prev-indexes (transient
-                       (reduce merge {}
-                               (map vector
-                                    (butlast nums)
-                                    (range (dec (count nums))))))
-         current (last nums)]
-    (if (= turn stop)
+(defn- generate-steps [init stop]
+  (loop [turn (count init)
+         prev (transient
+               (reduce merge {}
+                       (map vector
+                            (butlast init)
+                            (range (dec (count init))))))
+         current (last init)]
+    (if (>= turn stop)
       current
-      (let [prev-index (get prev-indexes current)]
-        (if (nil? prev-index)
-          (recur (inc turn)
-                 (assoc! prev-indexes current (dec turn))
-                 0)
-          (let [next-num (- (dec turn) prev-index)]
-            (recur (inc turn)
-                   (assoc! prev-indexes current (dec turn))
-                   next-num)))))))
+      (let [prev-turn (dec turn)
+            next-num (if-let [prev-index (get prev current)]
+                       (- prev-turn prev-index)
+                       0)]
+        (recur (inc turn)
+               (assoc! prev current prev-turn)
+               next-num)))))
 
-(defn solution-b [nums]
-  (ngen-seq nums 30000000))
+(defn solution-a [init]
+  (generate-steps init 2020))
+
+(defn solution-b [init]
+  (generate-steps init 30000000))
 
 (comment
   (solution-a sample) ; 436
