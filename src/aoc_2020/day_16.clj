461bd874e8cf0ad656499dd757791ba161f131c5
Johannes Groedem <johs@grdm.no>
2020-12-16 22:50:20 +0100

    Day 16: Start parsing.

--- src/aoc_2020/day_16.clj
+++ src/aoc_2020/day_16.clj
@@ -1,8 +1,29 @@
 (ns aoc-2020.day-16
-  (:require [aoc-2020.core :refer [slurp-lines]]))
+  (:require [aoc-2020.core :refer [slurp-groups parse-ints]]
+            [clojure.string :as str]))
 
-(defn solution-a [filename]
+(defn- parse-rules [lines]
   )
 
+(defn- parse-tickets [lines]
+  (let [[[header] tickets] (split-at 1 lines)]
+    (when (contains? #{"nearby tickets:" "your ticket:"} header)
+      (->> tickets
+           (map #(str/split % #","))
+           (map parse-ints)
+           (mapv vec)))))
+
+(defn- parse-notes [groups]
+  (let [[rules my-ticket nearby-tickets] groups]
+    {:rules (parse-rules rules)
+     :my-ticket (first (parse-tickets my-ticket))
+     :nearby-tickets (parse-tickets nearby-tickets)}))
+
+(defn solution-a [filename]
+  (-> filename
+      slurp-groups
+      parse-notes))
+
 (comment
+  (solution-a "sample-16.txt")
   )
