8a92541c811081d825af370d9c09862d4037909b
Johannes Groedem <johs@grdm.no>
2021-01-27 10:31:58 +0100

    Upgrade Clojure version.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://grdm.no/aoc-2020"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.10.2"]
                  [org.clojure/math.combinatorics "0.1.6"]
                  [org.clojure/data.json "1.0.0"]
                  [com.hypirion/primes "0.2.2"]]
